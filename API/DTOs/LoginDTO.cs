869b4e29cc9ede2fba3c9d1ff2402eea772dc1e7
Andrey Zhilyakov <Andrey.Zhilyakov@UBC.com>
2021-11-16 21:59:22 -0500

    adding authentication

---
+++ API/DTOs/LoginDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class LoginDTO
+    {
+        
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
